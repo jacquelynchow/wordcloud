eecff9a59fb456bdb6a555629d8250aa2c98d7a5
Jacquelyn Chow <icecreamjackie@gmail.com>
2020-08-22 15:58:12 -0400

    three more stories

--- README.md
+++ README.md
@@ -2,7 +2,22 @@
 *Description:* Displays and saves an image that's made up of different sized words (sizes of the words are determined by the frequency of each word in a specific text)
 
 ## Features:
-Wordcloud library (WordCloud generator and Stop Words)
+- Wordcloud library (WordCloud generator and Stop Words)
+- Instead of using Wordcloud's built in word cloud generator, I coded my own frequency dictionary for the word cloud
+- Masked image feature (example done for Scarlet Letter wordcloud)
 
 ## Texts Done So Far:
+*Text files from Project Gutenberg*
 - The Scarlet Letter (by Nathaniel Hawthorne)
+- The Wonderful Wizard of Oz (by L. Frank Baum)
+- The Iliad (by Homer)
+- Moby Dick; Or, The Whale (by Herman Melville)
+
+## How To Use With Your Own Text Files
+1) Add your own text file into the textfiles folder (remove any extra unwanted text in the file)
+In the .py file, 
+2) Add the name of the .txt file to the textName variable 
+3) Add your own stop words into the extraWords list
+4) Run the program by doing python3 word-cloud.py
+### Optional:
+Play around in the calculate_frequencies() with the parameters for WordCloud() -- the max_font_size, min_font_size, max_words -- or add more words to the extraWords list for a better word cloud display
--- word-cloud.py
+++ word-cloud.py
@@ -8,13 +8,13 @@ from IPython.display import display
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 from PIL import Image
 
-def calculate_frequencies(f):
+def calculate_frequencies(f, textName, extraWords):
     # Here is a list of punctuations and stop words you can use to process the text
-    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    punctuations = '''!()-[]“{};:"\,<>./?@#$%^&*_~'''
     stopwords = set(STOPWORDS)
     # extra words that don't have relavance to the text file but appear alot
-    stopwords.add('illustration')
-
+    for word in extraWords:
+        stopwords.add(word)
     # init the dictionary that will show frequency of words
     freqDict = {}
 
@@ -35,7 +35,7 @@ def calculate_frequencies(f):
             # end of the word, got either a punct. or space
             else:
                 # new word, need to save in dictionary
-                if (saveWord not in freqDict) and (saveWord not in stopwords):
+                if (saveWord not in freqDict) and (saveWord not in stopwords) and (len(saveWord) > 1):
                     freqDict[saveWord] = 1
                 # word already in dictionary
                 elif (saveWord in freqDict):
@@ -44,21 +44,43 @@ def calculate_frequencies(f):
                 # restart saved word var
                 saveWord = ""
     
-    scarlet_letter_coloring = np.array(Image.open("scarletBW.jpg"))
+    # for masking images
+    # image = "mask-images/" + textName + "BW.jpg"
+    # letter_coloring = np.array(Image.open(image))
+    # add "mask=letter_coloring," to WordCloud() parameters
 
     # wordcloud
-    cloud = WordCloud(background_color="white", max_words=1000, mask=scarlet_letter_coloring,
-               stopwords=stopwords, max_font_size=40, random_state=42)
+    cloud = WordCloud(background_color="white", width=600, height= 400, max_words=800, 
+               stopwords=stopwords, max_font_size=130, min_font_size=3, random_state=42, scale=3)
     cloud.generate_from_frequencies(freqDict)
+   
     # save wordcloud jpg
-    cloud.to_file("scarlet_wordcloud.jpg")
+    saveImage = textName + "_wordcloud.jpg"
+    cloud.to_file(saveImage)
     return cloud.to_array()
 
+# textName = "scarlet"
+# extraWords = ["illustration", "say", "thus", "might", "moreover", "days", "thought", "ago", "part", "one", "come", "mr", "said", "likewise", \
+#     "meanwhile", "yet", "perhaps", "stood", "-and", "rather", "nevertheless"]
 
-filename = "scarlet.txt"
+# textName = "iliad"
+# extraWords = ["pg", "said", "spoke", "thus", "thee", "threw", "know"]
+
+# textName = "oz"
+# extraWords = ["said", "asked", "know", "replied", "answered", "em", "knew"]
+
+# textName = "mobydick"
+# extraWords = ["say", "one", "said", "though", "oh", "come", "see", "mister", "meanwhile", "days" \
+#     "will", "besides", "mr", "side", "yet", "thing", "way", "look", "day", "indeed"]
+# filename = "textfiles/" + textName + ".txt"
+
+# INPUT DATA HERE
+textName = "input"
+extraWords = ["word"]
+
 # open text file
 f = open(filename, "r")
-myimage = calculate_frequencies(f.read())
+myimage = calculate_frequencies(f.read(), textName, extraWords)
 # close file
 f.close()
 
